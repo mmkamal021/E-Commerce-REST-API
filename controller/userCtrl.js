d69b5370a17958e9a9de93128f87c0b3d2f5bd20
mostafa-kamal <mmkamal021@gmail.com>
2023-03-01 16:47:43 +0600

    commited

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -69,8 +69,32 @@ const handleRefreshToken = asyncHandler(async (req, res) => {
       throw new Error('There is something wrong with refresh token')
     }
     const accessToken = generateToken(user?._id)
-    res.json(accessToken)
+    res.json({ accessToken })
   })
+})
+
+// logout functionality
+
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies
+  if (!cookie?.refreshToken) throw new Error('No Refresh Token in Cookies')
+  const refreshToken = cookie.refreshToken
+  const user = await User.findOne({ refreshToken })
+  if (!user) {
+    res.clearCookie('refreshToken', {
+      httpOnly: true,
+      secure: true,
+    })
+    return res.sendStatus(204) // forbidden
+  }
+  await User.findOneAndUpdate(refreshToken, {
+    refreshToken: '',
+  })
+  res.clearCookie('refreshToken', {
+    httpOnly: true,
+    secure: true,
+  })
+  res.sendStatus(204) // forbidden
 })
 
 // Update a user
@@ -196,4 +220,5 @@ module.exports = {
   blockUser,
   unblockUser,
   handleRefreshToken,
+  logout,
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -9,6 +9,7 @@ const {
   blockUser,
   unblockUser,
   handleRefreshToken,
+  logout,
 } = require('../controller/userCtrl')
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
 const router = express.Router()
@@ -17,6 +18,7 @@ router.post('/register', createUser)
 router.post('/login', loginUserCtrl)
 router.get('/all-users', getallUser)
 router.get('/refresh', handleRefreshToken)
+router.get('/logout', logout)
 router.get('/:id', authMiddleware, isAdmin, getaUser)
 router.delete('/:id', deleteaUser)
 // router.put('/:id', updateaUser) // using to --> req.param
